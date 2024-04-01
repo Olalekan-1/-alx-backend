c40a66ddf9314b2aebd3cafbf3ce5749dcbea19c
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-04-01 22:01:47 +0100

    add solution

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,5 +8,5 @@ def index_range(page, page_size):
     """ Returs index range of a page
     """
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
-    return start_index, end_index
+    end_index = start_index + page_size
+    return (start_index, end_index)
