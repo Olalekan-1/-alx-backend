0766caecd2ca9458d666535426618b89abc671f1
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-04-01 03:28:55 +0100

    add set up and Task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" An helper function
+"""
+
+
+def index_range(page, page_size):
+    """ Returs index range of a page
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size - 1
+    return start_index, end_index
