0f4e384d63a80fe01f102fbcf439c3843a70c066
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-04-10 01:37:52 +0100

    add solution

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+""" Set up python flask application """
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+import pytz
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """ Get the locale language """
+
+    if 'locale' in request.args:
+        requested_locale = request.args.get('locale')
+        if requested_locale in app.config['LANGUAGES']:
+            return requested_locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(user_id):
+    """ Gets user id """
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """ display before request """
+    user_id = request.args.get('login_as')
+    g.user = get_user(int(user_id)) if user_id else None
+
+
+@app.route('/')
+def index():
+    """ index html """
+    if g.user:
+        welcome_message = _("You are logged in as %(username)s.") \
+            % {'username': g.user['name']}
+    else:
+        welcome_message = _("You are not logged in.")
+    return render_template('index.html', welcome_message=welcome_message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
